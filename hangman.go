0d915974d018313394ed83c1a9cb8f698f901dc9
lmatheo <matheo.lang@ynov.com>
2022-10-12 14:09:46 +0200

    initgame

--- hangman.go
+++ hangman.go
@@ -11,16 +11,18 @@ import (
 
 func main() {
 	fmt.Println("Good Luck, you have 10 attempts.")
-	word, long := ChooseWord()
-	fmt.Println(word)
+	solution, long := ChooseWord()
+	fmt.Println(solution)
 	fmt.Println(long)
-	mot, attempts := InitGame(word)
-	fmt.Println(mot, attempts)
-	mott := ShowWord(mot)
-	fmt.Println(mott)
+	//mottab, attempts := InitGame(solution)
+	//fmt.Println(mottab)
+	//fmt.Println(attempts)
+	//gameword := ShowWord(mottab)
+	// fmt.Println(gameword)
+	// fmt.Println(len(gameword))
 }
 
-func ChooseWord() (string, int) {
+func ChooseWord() ([]string, int) {
 	name := os.Args[1]
 	body, err := ioutil.ReadFile(name)
 	if err != nil {
@@ -29,11 +31,12 @@ func ChooseWord() (string, int) {
 	list := []string{}
 	hold := ""
 	for _, m := range string(body) {
-		if m != 10 {
+		if m != rune(10) {
 			hold = hold + string(m)
 		} else {
 			if hold != "" {
 				list = append(list, hold)
+				fmt.Println(list)
 				hold = ""
 			}
 		}
@@ -41,7 +44,8 @@ func ChooseWord() (string, int) {
 	rand.Seed(time.Now().UnixNano())
 	list = append(list, hold)
 	lent := rand.Intn(len(list))
-	return list[lent], len(list[lent]) - 1
+	return list, len(list[lent])
+	//return list[lent], len(list[lent]) - 1
 }
 
 func InitGame(word string) ([]string, int) {
@@ -63,4 +67,16 @@ func ShowWord(word []string) string {
 		motstr += " " + ch
 	}
 	return motstr
+}
+
+func TabtoStr(word []string) string {
+	var str string
+	for _, ch := range word {
+		str += ch
+	}
+	return str
+}
+
+func PlayGame(solution string, mottab []string) {
+
 }
