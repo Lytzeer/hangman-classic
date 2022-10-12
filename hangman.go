f903cda2fd20910b23ae5b9dcb16a307e76ba3f3
Timothee DURAN <timothee.duran@ynov.com>
2022-10-12 14:41:54 +0200

    play works

--- hangman.go
+++ hangman.go
@@ -11,18 +11,19 @@ import (
 
 func main() {
 	fmt.Println("Good Luck, you have 10 attempts.")
-	solution, long := ChooseWord()
-	fmt.Println(solution)
-	fmt.Println(long)
-	//mottab, attempts := InitGame(solution)
-	//fmt.Println(mottab)
-	//fmt.Println(attempts)
-	//gameword := ShowWord(mottab)
-	// fmt.Println(gameword)
-	// fmt.Println(len(gameword))
+	word, long := ChooseWord()
+	fmt.Println(word)
+	var nouvmot string
+	for i:= 0; i< len(word) - 1; i++ {
+		nouvmot += string(word[i])
+	}
+	mot, attempts := InitGame(word)
+	mott := ShowWord(mot)
+	fmt.Println(mott)
+	Play(attempts, nouvmot, mot, long)
 }
 
-func ChooseWord() ([]string, int) {
+func ChooseWord() (string, int) {
 	name := os.Args[1]
 	body, err := ioutil.ReadFile(name)
 	if err != nil {
@@ -31,12 +32,11 @@ func ChooseWord() ([]string, int) {
 	list := []string{}
 	hold := ""
 	for _, m := range string(body) {
-		if m != rune(10) {
+		if m != 10 {
 			hold = hold + string(m)
 		} else {
 			if hold != "" {
 				list = append(list, hold)
-				fmt.Println(list)
 				hold = ""
 			}
 		}
@@ -44,8 +44,7 @@ func ChooseWord() ([]string, int) {
 	rand.Seed(time.Now().UnixNano())
 	list = append(list, hold)
 	lent := rand.Intn(len(list))
-	return list, len(list[lent])
-	//return list[lent], len(list[lent]) - 1
+	return list[lent], len(list[lent]) - 1
 }
 
 func InitGame(word string) ([]string, int) {
@@ -61,6 +60,36 @@ func InitGame(word string) ([]string, int) {
 	return mot, 10
 }
 
+func Play(attempts int, word string, mottab []string, long int) {
+	var present bool
+	var letter string
+	for word != TabtoStr(mottab) {
+		if attempts == 0 {
+			fmt.Println("Game over! The correct word was", word)
+			return
+		} else {
+			present = false
+			fmt.Print("Choose: ")
+			fmt.Scan(&letter)
+			for i := 0; i < len(word); i++ {
+				if string(word[i]) == letter {
+					mottab[i] = letter
+					present = true
+				}
+			}
+		}
+		if !present {
+            attempts--
+            if attempts >= 1 {
+                fmt.Println("Not present in the word, ", attempts, " attempts remaining")
+			}
+		}
+        fmt.Println(TabtoStr(mottab))
+	}
+	fmt.Println("Congrats !")
+}
+
+
 func ShowWord(word []string) string {
 	var motstr string
 	for _, ch := range word {
@@ -70,13 +99,9 @@ func ShowWord(word []string) string {
 }
 
 func TabtoStr(word []string) string {
-	var str string
-	for _, ch := range word {
-		str += ch
-	}
-	return str
-}
-
-func PlayGame(solution string, mottab []string) {
-
+    str := ""
+    for _, ch := range word {
+        str += ch
+    }
+    return str
 }
